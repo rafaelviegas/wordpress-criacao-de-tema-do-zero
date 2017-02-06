038cb1eab2f5400979fd4bf3fabecc0c00199cc6
Rafael Viegas <rafael.viegas@arpagus.com.br>
2017-02-06 08:57:18 -0200

    02. Cadastrando os Imoveis

---
+++ app/wp-content/themes/customizado/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <?php $root = get_template_directory_uri();?>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="<?=$root;?>/reset.css">
+    <link rel="stylesheet" type="text/css" href="<?=$root;?>/style.css">
+    <title>Index</title>
+    <?php wp_head(); ?>
+</head>
+<body>
--- app/wp-content/themes/customizado/index.php
+++ app/wp-content/themes/customizado/index.php
@@ -1,10 +1,24 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <title>Index</title>
-</head>
-<body>
-    <h1>Bem-vindo!</h1>
-</body>
-</html>
+<?php get_header(); ?>
+
+<main class="home-main">
+    <div class="container">
+        <h1>Bem-vindo!</h1>
+        <ul class="imoveis-listagem">
+            <?php 
+                if (have_posts()){
+                    while (have_posts()){
+                        the_post(); 
+            ?>
+                <li class="imoveis-listagem-item">
+                        <?php the_post_thumbnail() ?>
+                        <h1><?php the_title();?></h1>
+                        <div><?php the_content();?></div>    
+                </li>
+            <?php            
+                    } 
+                } 
+            ?>
+        </ul>
+    </div>
+</main>
+<?php get_footer(); ?>
